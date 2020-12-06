5e71c95ef7a7ad95c7bee438ef22b2a87de7196c
Fauzanjaludin <73268282+Fauzanjaludin@users.noreply.github.com>
2020-12-06 16:45:35 +0800

    Upload

---
+++ Exercise4/src/abstractb/Exercise3a.java
@@ -0,0 +1,31 @@
+package abstractb;
+
+abstract class parent{
+	void message() {
+		System.out.println("MESSAGE");
+	}
+		
+}
+
+class firstSC extends parent{
+	void message() {
+		System.out.println("This is first subclass");
+	}
+}
+
+class secSC extends parent{
+	void message() {
+		System.out.println("This is second subclass");
+	}
+}
+
+public class Exercise3a {
+
+	public static void main(String[] args) {
+		firstSC fsc=new firstSC();
+		secSC ssc=new secSC();
+		
+		fsc.message();
+		ssc.message();
+	}
+}
---
+++ Exercise4/src/abstractb/Exercise3b.java
@@ -0,0 +1,38 @@
+package abstractb;
+
+abstract class bank{
+	abstract void getBalance();
+}
+
+class bankA extends bank{
+	void getBalance() {
+		System.out.println("Balance Bank A:100");
+	}
+}
+
+class bankB extends bank{
+	void getBalance() {
+		System.out.println("Balance Bank B:150");
+	}
+}
+
+class bankC extends bank{
+	void getBalance() {
+		System.out.println("Balance Bank C:200");
+	}
+}
+
+public class Exercise3b {
+
+	public static void main(String[] args) {
+		bankA a=new bankA();
+		bankB b=new bankB();
+		bankC c=new bankC();
+		
+		a.getBalance();
+		b.getBalance();
+		c.getBalance();
+		
+		
+	}
+}
